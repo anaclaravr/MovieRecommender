83f560ddd5cc3f77006bd7617c64ea69b459b26b
Jennifer <jennifercsilva28@gmail.com>
2026-05-15 01:22:08 -0300

    Aprimoramento da barra de pesquisa

--- src/app/core/api/api.config.ts
+++ src/app/core/api/api.config.ts
@@ -1,2 +1,6 @@
+// Backend local
+ //export const API_BASE_URL = 'http://127.0.0.1:8000/api/v1';
+
+// Backend publicado
 export const API_BASE_URL =
   'https://pbr-si-2026-1-p8-tcc-t1-30105-pbr-si.onrender.com/api/v1';
